45ecfdd0e7b979ce82a86c84ac05608dca6b72d6
unknown <gonzcarjuan06@gmail.com>
2025-11-07 18:42:49 +0000

    feat: added the colors sheet and the header

--- app/index.tsx
+++ app/index.tsx
@@ -1,19 +1,35 @@
 
-import { StyleSheet, Text, View } from 'react-native';
+import { FlatList, StyleSheet, Text, View } from 'react-native';
+import { colors } from '../styles/Colors';
 
 export default function App() {
   return (
-    <View>
-        <Text>wow</Text>
-    </View>
+    <>
+      <View style={styles.header}><Text style={styles.headerText}>Lista Videojuegos</Text></View>
+      <View style={styles.temporalContainer}></View>
+      <View style={styles.temporalContainer2}></View>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  header: {
+    flex: 10,
+    padding: 20,
+    backgroundColor: colors.secondary
   },
+  headerText: {
+    marginLeft: 30,
+    marginTop: 25,
+    color: colors.text,
+    fontSize: 35
+  },
+  temporalContainer: {
+    flex: 80,
+    backgroundColor: colors.primary
+  },
+  temporalContainer2: {
+    flex: 10,
+    backgroundColor: colors.secondary
+  }
 });
